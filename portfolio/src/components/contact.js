1da9e5c729dea8386e5a078ca46a9ecd5a3168aa
hhundley <65366145+hhundley@users.noreply.github.com>
2022-11-17 12:30:01 -0600

    added js functionality to contact section with emailjs

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -1,40 +1,37 @@
-import React from 'react'
+import React,{useRef} from 'react';
 import '../styles/contact.css';
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-  const [formStatus, setFormStatus] = React.useState('Send_')
-  const onSubmit = (e) => {
+    const form = useRef();
+    const [formStatus, setFormStatus] = React.useState('Send_')
+    const onSubmit = (e) => {
     e.preventDefault()
-    setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }
-    console.log(conFom)
+
+    emailjs.sendForm('service_8iek2zr','template_hbzjwh7',form.current,'juroUeYqFqyKEL-IM')
+    setFormStatus('⚡ Sent ⚡')
   }
   return (
     <div className="container mt-5">
       <h2 className="mb-3">Contact</h2>
-      <form onSubmit={onSubmit}>
+      <form ref={form} onSubmit={onSubmit}>
         <div className="mb-3">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input className="form-control" type="text" id="name" required />
+          <input className="form-control" type="text" name="name" required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="email">
             Email
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input className="form-control" type="email" name="email" required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="message">
             Message
           </label>
-          <textarea className="form-control" id="message" required />
+          <textarea className="form-control" name="message" required />
         </div>
         <button id="submitbtn" className="btn btn-danger" type="submit">
           {formStatus}
