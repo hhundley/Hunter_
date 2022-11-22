640c73c384fc0e2923fc180197ec5a7c86ee4189
hhundley <65366145+hhundley@users.noreply.github.com>
2022-11-22 09:27:50 -0600

    work section styling changes

--- src/components/work.js
+++ src/components/work.js
@@ -8,9 +8,18 @@ function Work() {
           <div>
             <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://whispering-cove-28179.herokuapp.com/" target="_blank" rel="noopener noreferrer">Ticket Scholar</a></span></p>
             <p id="project-description">a concert finder.</p>
-        
+
             <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://hhundley.github.io/simple-weather-app/" target="_blank" rel="noopener noreferrer">Weather</a></span></p>
             <p id="project-description">a simple, sleek weather app.</p>
+
+            <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://secret-scrubland-70160.herokuapp.com/" target="_blank" rel="noopener noreferrer">The Text Tool</a></span></p>
+            <p id="project-description">a simple text editor in the form of a PWA.</p>
+
+            <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://github.com/hhundley/social-network-api.git" target="_blank" rel="noopener noreferrer">Social Network API</a></span></p>
+            <p id="project-description">an api and non-relational database built for a social network.</p>
+
+            <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://github.com/hhundley/Ecommerce-Practice.git" target="_blank" rel="noopener noreferrer">Ecommerce Backend</a></span></p>
+            <p id="project-description">a backend and relational database for an ecommerce platform.</p>
 
             <p id="work-p"><span className="bigger-text" id="work-link">Under Construction</span></p>
             <p id="project-description">a placeholder for a custom designed bike shop.</p>
