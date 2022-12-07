0fa243c683b61d17b2399bae0f887ebfe9bf3253
hhundley <65366145+hhundley@users.noreply.github.com>
2022-12-07 16:58:15 -0600

    rearranging work

--- src/components/work.js
+++ src/components/work.js
@@ -9,14 +9,14 @@ function Work() {
             <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://safe-ravine-42690.herokuapp.com/" target="_blank" rel="noopener noreferrer">Code Kings</a></span></p>
             <p id="project-description">a community led project sharing app.</p>
 
+            <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://secret-scrubland-70160.herokuapp.com/" target="_blank" rel="noopener noreferrer">The Text Tool</a></span></p>
+            <p id="project-description">a simple text editor in the form of a PWA.</p>
+
             <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://whispering-cove-28179.herokuapp.com/" target="_blank" rel="noopener noreferrer">Ticket Scholar</a></span></p>
             <p id="project-description">a concert finder.</p>
 
             <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://hhundley.github.io/simple-weather-app/" target="_blank" rel="noopener noreferrer">Weather</a></span></p>
             <p id="project-description">a simple, sleek weather app.</p>
-
-            <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://secret-scrubland-70160.herokuapp.com/" target="_blank" rel="noopener noreferrer">The Text Tool</a></span></p>
-            <p id="project-description">a simple text editor in the form of a PWA.</p>
 
             <p id="work-p"><span className="bigger-text"><a id="work-link" href="https://github.com/hhundley/social-network-api.git" target="_blank" rel="noopener noreferrer">Social Network API</a></span></p>
             <p id="project-description">an api and non-relational database built for a social network.</p>
