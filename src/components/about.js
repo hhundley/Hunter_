8758e027bac6cb481d54babca76ed7baa3234695
hhundley <65366145+hhundley@users.noreply.github.com>
2023-02-15 16:18:38 -0600

    changing about to add titles

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ import hunter from '../images/hunter.png';
 function About() {
     return (
         <section className="container mt-5">
-            <p className="animate__animated animate__fadeInUp" id="business"><span className="bigger">Hey there! </span><br></br><br></br>I'm a full stack developer building a wide variety of applications. I focus on efficient, scalable, and functional design. I am motivated by a love of building and venturing into the unknown.<br></br><br></br>I'm a product of my environment. Raised in the great city of Austin, TX, you can often find me running around town lake or maybe at Hopdoddy checking out the burger of the month. If you would like to say hi or go grab a coffee sometime, don't hesitate to reach out!</p>
+            <p className="animate__animated animate__fadeInUp" id="business"><span className="bigger">Hey there! </span><br></br><br></br>I'm a prodct designer and full stack developer building a wide variety of applications. I focus on efficient, scalable, and functional design. I am motivated by a love of building and venturing into the unknown.<br></br><br></br>I'm a product of my environment. Raised in the great city of Austin, TX, you can often find me running around town lake or maybe at Hopdoddy checking out the burger of the month. If you would like to say hi or go grab a coffee sometime, don't hesitate to reach out!</p>
                 <div className="animate__animated animate__fadeInUp">
                 <div>
                     <img id="webpic" src={hunter} className="hunter" alt="hunter" />
